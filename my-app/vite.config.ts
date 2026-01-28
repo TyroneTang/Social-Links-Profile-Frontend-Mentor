2e91eb61a7da73fcc082a46890e5c462c3aad415
Tyrone Tang <tyronetang.xi@yahoo.com>
2026-01-28 16:28:59 +0000

    rebuild docs

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "Social-Links-Profile-Frontend-Mentor",
+  base: "/Social-Links-Profile-Frontend-Mentor/",
   build: {
     outDir: "docs"
   },
